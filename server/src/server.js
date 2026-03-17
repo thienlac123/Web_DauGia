62d2fdbcbbcd9abbff87507c2cdf9d95e7f90356
Lac Vu <lacvu2221@gmail.com>
2026-03-17 10:29:45 +0700

    fix file

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,21 @@
-require("dotenv").config();
-const http = require("http");
-const { Server } = require("socket.io");
-const app = require("./app");
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
@@ -21,6 +26,14 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error.message);
+  });
